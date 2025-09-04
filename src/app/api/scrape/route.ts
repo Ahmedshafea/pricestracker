703a08a5c13eb4d7b9d395077c59ee7953131aaa
AhmedShafea <ahmedshafea@outlook.com>
2025-09-04 12:00:10 +0300

    remove any

--- src/app/api/scrape/route.ts
+++ src/app/api/scrape/route.ts
@@ -38,7 +38,9 @@ export async function GET(req: Request) {
 
     await browser.close();
     return NextResponse.json(data);
-  } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
+  } catch (error) {
+  const message = error instanceof Error ? error.message : "Unknown error";
+  return NextResponse.json({ error: message }, { status: 500 });
+}
+
 }
